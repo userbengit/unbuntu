6478add0f3b7d992744bffe0e01b2dc5805688fa
bensinh <ben.transinh@gmail.com>
2022-05-21 14:53:59 +0700

    list python

---
+++ list.py
@@ -0,0 +1,17 @@
+colors = ["red", "green", "blue"]
+
+print(colors)
+print(colors[0])
+
+print(len(colors))
+
+colors.append("yellow")
+
+print(colors)
+
+for i in range(4):
+	print(colors[i])
+
+print(len(colors))
+
+print(colors[len(colors)-1])
